83965e392131fb79946e52c725ef8dc3e90dee87
Thomas Goodman <tmg2134@truman.edu>
2018-05-22 01:01:56 -0500

    Initial commit
    Start of protoype for action tap game

---
+++ Assets/scripts/EnemyController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour {
+
+  public float moveSpeed;
+  public int damage;
+  public int hitpoints;
+  public float interactDistance;
+  public float bounceDist;
+  public GameObject thePlayer;
+  public float bounceFramesTotal;
+
+  float bounceFramesLeft;
+
+  Rigidbody2D rigidBody;
+  BoxCollider2D collider;
+
+
+  // Animator enemyAnimator;
+  // CharacterController controller;
+
+	// initialization
+	void Start () {
+		collider = GetComponentInChildren<BoxCollider2D>();
+    rigidBody = GetComponent<Rigidbody2D>();
+	}
+	
+	void Update () {
+	  float theMag = (transform.position - thePlayer.transform.position ).magnitude;
+    if (theMag <= interactDistance && bounceFramesLeft <= 0){
+      collider.isTrigger = true;
+    } else {
+      collider.isTrigger = false;
+      if(bounceFramesLeft > 0){
+        Debug.Log(rigidBody.velocity);
+        bounceFramesLeft -= 1;
+      }
+    }
+
+    if(bounceFramesLeft <= 0){
+      transform.position += Vector3.right * -1 * moveSpeed * Time.deltaTime;
+    }
+    
+	}
+
+  public void Bounce(){
+    // transform.position += Vector3.right * bounceDist;
+    bounceFramesLeft = bounceFramesTotal;
+    collider.isTrigger = false;
+    rigidBody.AddForce(Vector3.right * bounceDist);
+  }
+
+  public int getDamage(){
+    return damage;
+  }
+
+}
---
+++ Assets/scripts/PlayerController.cs
@@ -0,0 +1,145 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlayerController : MonoBehaviour {
+
+  float abilitySize;
+  public float hitpoints;
+  Animator playerAnimator;
+  
+  CharacterController controller;
+
+  List<Ability> playerAbilities;
+
+	// Initialization
+	void Start () {
+    controller =  GetComponent<CharacterController>();
+    // weaponCollider = GetComponentInChildren<CheckWeaponCollision>();
+    playerAnimator = GetComponent<Animator>();
+
+    // input not needed? Auto move
+    // input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+    // inputDir = inputDirut.normalized;
+
+    // targetRotation = Mathf.Atan2(inputDir.x, inputDir.y) * Mathf.Rad2Deg + cameraT.eulerAngles.y;
+
+	}
+	
+	void Update () {
+
+    // List<Touch> touchList = Input.touches;
+
+    // List of touches that will be used for attacks
+    List<Touch> touchList = new List<Touch>();
+
+    // Iterate through touches. 
+    // If a touch is an Ability, use it, and do not use for attack
+    // If a touch clicked a menu item, disable all touch attacks (return empty list) and activate menu
+
+    touchList = checkTouches(Input.touches);
+
+    if(touchList != null){
+      Attack(touchList);
+    } else {
+      Defend();
+    }
+
+	}
+
+  public void Defend(){
+    // Put up shield for defending projectiles and enemy attacks
+  }
+
+  public void Attack(List<Touch> attackTouches){
+    // playAttack animation based on tap and and number of 
+  }
+
+
+  public void UseAbility(Ability theAbility){
+    // get ability used
+    // abilityNum = theAbility.whichAbility;
+    // do ability stuff
+    // allAbilitiesList[abilityNum].playAbility();
+  }
+
+
+  /* 
+    Only want to count player's touches if they are not clicking on a menu item or ability
+    Check if touching abilites
+    Check if touching the menu
+    Return the amount of touches not touching these
+  */
+  public List<Touch> checkTouches(Touch[] touchArray){
+    // int fingerCount = 0;
+    List<Touch> leftOverTouches = new List<Touch>();
+    foreach (Touch touch in touchArray) {
+      if (touch.phase != TouchPhase.Ended && touch.phase != TouchPhase.Canceled){
+        // Check if player has abilities, and if so, iterate
+        bool usedTouch = false;
+        if(playerAbilities != null){
+          foreach (Ability someAbility in playerAbilities){
+          // if within bounds of ability position
+            // someAbility.useAbility()
+            // usedTouch = true
+          }
+        }
+        // foreach (MenuItem mItem in menuOptions)
+          // if within bounds
+            // goto menu item, disable touch for damage
+            // usedTouch = true
+        if(usedTouch == false){
+          leftOverTouches.Add(touch);
+          // fingerCount++;
+        }
+      }
+    } 
+    return leftOverTouches;
+  }
+
+  public void AddAbility(Vector3 position, float cooldown, int abilityNum){
+    Ability newAbility = new Ability(position, cooldown, abilityNum);
+    playerAbilities.Add(newAbility);
+  }
+
+  public void takeDamage(int damage){
+    hitpoints -= damage;
+  }
+
+  void OnTriggerStay2D(Collider2D other) {
+    Debug.Log("HELLO?");
+    if(other.gameObject.tag == "Enemy"){
+      EnemyController enemyController = other.gameObject.GetComponentInParent<EnemyController>();
+      enemyController.Bounce();
+      takeDamage(enemyController.getDamage());
+      Debug.Log(hitpoints);
+    }
+  }
+
+}
+
+public struct Ability {
+  public readonly Vector3 position;
+  public readonly float cooldown;
+  public readonly int abilityNum;
+  public float lastUsed;
+
+  public Ability (Vector3 position, float cooldown, int abilityNum)
+  {
+    this.position = position;
+    this.cooldown = cooldown;
+    this.abilityNum = abilityNum;
+    lastUsed = 0;
+  }
+
+  public void useAbility(float frame){
+    if(lastUsed + cooldown > frame){
+      lastUsed = frame;
+      // Do Ability stuff
+    } else {
+      Debug.Log("Still on cooldown");
+    }
+    
+  }
+
+}
